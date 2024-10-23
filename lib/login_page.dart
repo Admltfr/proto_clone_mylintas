a5375038e43047d3e3b9b00969e53247888d65af
admltfr <adamlutfiramadhan@gmail.com>
2024-10-23 10:35:12 +0700

    added submit button

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,13 +6,12 @@ void main() {
 }
 
 class LoginPage extends StatelessWidget {
-  LoginPage({super.key});
-
   final _codeKey = GlobalKey<FormState>();
 
   final TextEditingController emailController = TextEditingController();
 
   final TextEditingController passwordController = TextEditingController();
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +26,7 @@ class LoginPage extends StatelessWidget {
               Stack(
                 alignment: Alignment.center,
                 children: [
-                  Container(
+                  SizedBox(
                       width: MediaQuery.sizeOf(context).width,
                       height: MediaQuery.sizeOf(context).height / 3,
                       child: Opacity(
@@ -72,6 +71,7 @@ class LoginPage extends StatelessWidget {
                           width: MediaQuery.sizeOf(context).width,
                           height: MediaQuery.sizeOf(context).width / 8,
                           child: TextFormField(
+                            
                             controller: emailController,
                             decoration: InputDecoration(
                               prefixIcon: Icon(
@@ -93,12 +93,18 @@ class LoginPage extends StatelessWidget {
                                 ),
                               ),
                             ),
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return '';
+                              }
+                              return null;
+                            },
                           ),
                         ),
                         Container(
                           margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
                           width: MediaQuery.sizeOf(context).width,
-                          height: 40,
+                          height: MediaQuery.sizeOf(context).width / 8,
                           child: TextFormField(
                             controller: passwordController,
                             obscureText: true,
@@ -122,70 +128,84 @@ class LoginPage extends StatelessWidget {
                                 ),
                               ),
                             ),
+                            
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return '';
+                              }
+                              return null;
+                            },
                           ),
-                        )
+                        ),
                       ],
                     ),
                   )),
               Container(
+                  width: MediaQuery.sizeOf(context).width,
+                  height: MediaQuery.sizeOf(context).width / 8,
                   alignment: Alignment.centerRight,
                   margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
                   color: Colors.transparent,
                   child: TextButton(
                       onPressed: () {},
-                      style: TextButton.styleFrom(
-                        splashFactory: NoSplash.splashFactory,
-                        foregroundColor: Colors.transparent,
-                      ),
                       child: Text(
                         "Lupa Password?",
                         style: TextStyle(color: Colors.black, fontSize: 13),
                       ))),
               Container(
                 margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green.shade500,
-                      shape: BeveledRectangleBorder()),
-                  onPressed: () {
-                    if (_codeKey.currentState!.validate()) {
-                      if (emailController.text == "admin123@gmail.com" &&
-                          passwordController.text == "admin123") {
-                        Navigator.pushReplacement(context,
-                            MaterialPageRoute(builder: (context) {
-                          return MainPage();
-                        }));
+                child: Builder(builder: (BuildContext context) {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green.shade500,
+                        shape: BeveledRectangleBorder()),
+                    onPressed: () {
+                      if (_codeKey.currentState!.validate()) {
+                        if (emailController.text == "admin123@gmail.com" &&
+                            passwordController.text == "admin123") {
+                          Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) {
+                            return MainPage();
+                          }));
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                                duration: Duration(seconds: 1),
+                                margin: EdgeInsets.all(10),
+                                behavior: SnackBarBehavior.floating,
+                                backgroundColor: Colors.pink.shade100,
+                                content: Text(
+                                  'Email atau password salah',
+                                  style: TextStyle(color: Colors.pink.shade900),
+                                )),
+                          );
+                        }
                       } else {
                         ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(
+                              duration: Duration(seconds: 1),
                               margin: EdgeInsets.all(10),
                               behavior: SnackBarBehavior.floating,
                               backgroundColor: Colors.pink.shade100,
-                              content: Text(
-                                'Email atau password salah',
-                                style: TextStyle(color: Colors.pink.shade900),
-                              )),
+                              content: Text('Email/password tidak boleh kosong',
+                                  style:
+                                      TextStyle(color: Colors.pink.shade900))),
                         );
                       }
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(
-                            content: Text('Email/password tidak boleh kosong')),
-                      );
-                    }
-                  },
-                  child: Container(
-                    alignment: Alignment.center,
-                    width: MediaQuery.sizeOf(context).width,
-                    height: 25,
-                    decoration: BoxDecoration(
-                        color: Colors.transparent, shape: BoxShape.rectangle),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.sizeOf(context).width,
+                      height: 25,
+                      decoration: BoxDecoration(
+                          color: Colors.transparent, shape: BoxShape.rectangle),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
                     ),
-                  ),
-                ),
+                  );
+                }),
               ),
             ],
           ),
